3074aeecb3799dc4923cb9ea41ba78b6623e538d
Arturito <arturaskisielius91@gmail.com>
2022-03-29 18:44:56 +0300

    variables.js

---
+++ js/variables.js
@@ -0,0 +1,50 @@
+/*
+KINTAMIEJI
+
+Iniciavimo budas:
+const - default iniciavimo budas
+let - antrinis budas inicijuoti, kai zinome, jog reiksme tures keistis
+var - pats seniausias ir geriau niekada nenaudoti, jei rupi saugumas
+
+Duomenu tipas: 
+- number, 
+- string (tekstas)
+- boolean (logine reiksme)
+-array (masyvas, sarasas, matrica*)
+
+*/
+
+const luckyNumber = 7;
+console.log('Lucky number', luckyNumber);
+
+let pinigine = 0;
+console.log('pinigine', pinigine);
+
+pinigine = 5;
+console.log('pinigine', pinigine);
+
+const vardas = 'Maryte';
+const pasisveikinimas = 'Laba diena, ' + vardas + '.';
+console.log(pasisveikinimas);
+
+nieko = 'nieko';
+console.log(nieko);
+
+const arVedes = true;
+const arTuriVaiku = false;
+const arMeluojaDelVaiku = true;
+console.log('Ar vedes?', arVedes);
+console.log('Ar meluoja?', arMeluojaDelVaiku);
+console.log('turi vaiku?', arTuriVaiku);
+
+console.log(2 + 2 * 2);
+console.log('2 + 2 * 2');
+
+const pazymiai = [10, 2, 8, 4, 6];
+console.log('Pazymiai:', pazymiai);
+
+const studentai = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+console.log(studentai);
+
+const booleanList = [true, false, true, false, true, false];
+console.log(booleanList);
