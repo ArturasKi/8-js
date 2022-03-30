2ee1011ce2d92d9e51c5984a64d9da4f52e2db79
Arturito <arturaskisielius91@gmail.com>
2022-03-30 11:09:35 +0300

    Numbers

---
+++ js/01-nd.js
@@ -0,0 +1,57 @@
+/*
+KINTAMUJU INICIJAVIMAS
+*/
+
+const numberOne = 1;
+console.log(numberOne);
+const numberTwo = 2;
+console.log(numberTwo);
+const numberThree = 3;
+console.log(numberThree);
+
+const vardenis = 'Arturas';
+console.log(vardenis);
+const pavardenis = 'Kisielius';
+console.log(pavardenis);
+const sokis = 'Lindyhopas';
+console.log(sokis);
+
+const metai = [1991, 2005, 2009, 2015, 2022];
+console.log(metai);
+const numeriai = [2, 4, 2, 3, 5];
+console.log(numeriai);
+const kiekiai = [20, 30, 40, 50, 60];
+console.log(kiekiai);
+
+const miestai = ['Alytus', 'Vilnius', 'Kaunas', 'Trakai', 'Varena'];
+console.log(miestai);
+const gyvunai = ['arklys', 'suo', 'lape', 'vilkas', 'meskenas'];
+console.log(gyvunai);
+const skaiciai = ['vienas', 'du', 'trys', 'keturi', 'penki'];
+console.log(skaiciai);
+
+/*
+VEIKSMAI SU KINTAMAISIAIS
+*/
+
+const suma = numberOne + numberTwo + numberThree;
+console.log(suma);
+const sumaTekstai = vardenis + ' ' + pavardenis + ' ' + sokis;
+console.log(sumaTekstai);
+
+const skaiciavimas = [2 - 4 + 2 - 3 + 5];
+console.log(skaiciavimas);
+
+
+console.log(skaiciai, gyvunai, miestai);
+
+
+
+
+
+
+
+
+
+
+
